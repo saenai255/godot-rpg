d6e90dd26b4f8786dfec738a66eb2c27c7d75255
Paul Cosma <paul.cosma97@gmail.com>
2025-03-26 15:46:42 +0200

    added item slots and weapon damage

--- autoloads/consts.gd
+++ autoloads/consts.gd
@@ -14,80 +14,95 @@ func _atlas_tex(src: Texture2D, region: Rect2) -> Texture2D:
 	return atlas
 
 
-var ITEM_PLANT_FIBER := Item.new(
-	1,
-	"Plant Fiber",
-	Item.ItemQuality.Common,
-	"Thin, flexible fibers harvested from plants, "
-	+ "ideal for crafting basic clothing or ropes.",
-	LARGE_STACK,
-	_atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 1, 24 * 9, 24, 24))
+var ITEM_PLANT_FIBER := Item.from(
+	func(item: Item) -> void:
+		item.name = "Plant Fiber"
+		item.quality = Item.ItemQuality.Common
+		item.description = "Thin, flexible fibers harvested from plants, ideal for crafting basic clothing or ropes."
+		item.stack_size = LARGE_STACK
+		item.texture = 	_atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 1, 24 * 9, 24, 24))
 )
 
-
-
-var ITEM_WOOD_LOG := Item.new(
-	2,
-	"Wood Log",
-	Item.ItemQuality.Common,
-	"A simple log gathered from trees, "
-	+ "useful for crafting basic tools and weapons.",
-	LARGE_STACK,
-	_atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 0, 0, 24, 24))
+var ITEM_WOOD_LOG := Item.from(
+	func(item: Item) -> void:
+		item.name = "Wood Log"
+		item.quality = Item.ItemQuality.Common
+		item.description = "A simple log gathered from trees, useful for crafting basic tools and weapons."
+		item.stack_size = LARGE_STACK
+		item.texture = _atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 0, 0, 24, 24))
 )
 
-
-var ITEM_ROUGH_STONE := Item.new(
-	3,
-	"Rough Stone",
-	Item.ItemQuality.Common,
-	"A rough piece of stone, commonly used to create "
-	+ "simple weapons and build fires.",
-	LARGE_STACK,
-	_atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 4, 0, 24, 24))
+var ITEM_ROUGH_STONE := Item.from(
+	func(item: Item) -> void:
+		item.name = "Rough Stone"
+		item.quality = Item.ItemQuality.Common
+		item.description = "A rough piece of stone, commonly used to create simple weapons and build fires."
+		item.stack_size = LARGE_STACK
+		item.texture = _atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 4, 0, 24, 24))
 )
 
-var ITEM_ROPE := Item.new(
-	4,
-	"Rope",
-	Item.ItemQuality.Uncommon,
-	"A piece of rope made out of fiber.",
-	LARGE_STACK,
-	_atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 3, 24 * 6, 24, 24))
+var ITEM_ROPE := Item.from(
+	func(item: Item) -> void:
+		item.name = "Rope"
+		item.quality = Item.ItemQuality.Uncommon
+		item.description = "A piece of rope made out of fiber."
+		item.stack_size = LARGE_STACK
+		item.texture = _atlas_tex(TEX_CRAFTING_MATS, Rect2(24 * 3, 24 * 6, 24, 24))
 )
 
-
-var ITEM_STONE_SWORD := Item.new(
-	5,
-	"Stone Sword",
-	Item.ItemQuality.Uncommon,
-	"Heavy and not very sharp. Probably not the best type of sword.",
-	UNSTACKABLE,
-	preload("res://sprites/stone_sword_transparent_32x32.png"),
-	Stats.from([
-		Stats.StatValue.new(Stats.Stat.Strength, 3),
-		Stats.StatValue.new(Stats.Stat.PhysicalPower, 5),
-		Stats.StatValue.new(Stats.Stat.CriticalPower, 10),
-	])
+var ITEM_STONE_SWORD := Item.from(
+	func(item: Item) -> void:
+		item.name = "Stone Sword"
+		item.quality = Item.ItemQuality.Uncommon
+		item.description = "Heavy and not very sharp. Probably not the best type of sword."
+		item.texture = preload("res://sprites/stone_sword_transparent_32x32.png")
+		item.stats = Stats.from([
+			Stats.StatValue.new(Stats.Stat.Strength, 3),
+			Stats.StatValue.new(Stats.Stat.PhysicalPower, 5),
+			Stats.StatValue.new(Stats.Stat.CriticalPower, 10),
+		])
+		item.slot = Item.EquipmentSlot.Weapon
+		item.weapon_slot = Item.WeaponSlot.TwoHand
+		item.damage = Item.DamageRange.new(3, 5, Item.DamageType.Physical)
 )
 
-var ITEM_THUNDERFURY := Item.new(
-	6,
-	"Thunderfury, Blessed Blade of the Windseeker",
-	Item.ItemQuality.Legendary,
-	"Probably a copyright infringement.",
-	UNSTACKABLE,
-	preload("res://sprites/stone_sword_transparent_32x32.png"),
-	Stats.from([
-		Stats.StatValue.new(Stats.Stat.Strength, 33),
-		Stats.StatValue.new(Stats.Stat.Stamina, 27),
-		Stats.StatValue.new(Stats.Stat.Intellect, 15),
-		Stats.StatValue.new(Stats.Stat.Dexterity, 17),
-		Stats.StatValue.new(Stats.Stat.CriticalRating, 43),
-		Stats.StatValue.new(Stats.Stat.CriticalPower, 152),
-		Stats.StatValue.new(Stats.Stat.DodgeRating, 72),
-		Stats.StatValue.new(Stats.Stat.MagicArmor, 52),
-	])
+var ITEM_SABIUTA_LU_MARIUCA := Item.from(
+	func(item: Item) -> void:
+		item.name = "Sabiuta lu Mariuca"
+		item.quality = Item.ItemQuality.Epic
+		item.description = "Da' ce-oi face?"
+		item.texture = preload("res://sprites/stone_sword_transparent_32x32.png")
+		item.stats = Stats.from([
+			Stats.StatValue.new(Stats.Stat.DodgeRating, 300),
+			Stats.StatValue.new(Stats.Stat.Intellect, 200),
+			Stats.StatValue.new(Stats.Stat.CriticalRating, 250),
+			Stats.StatValue.new(Stats.Stat.CriticalPower, 350),
+			Stats.StatValue.new(Stats.Stat.Stamina, 350),
+		])
+		item.slot = Item.EquipmentSlot.Weapon
+		item.weapon_slot = Item.WeaponSlot.OneHand
+		item.damage = Item.DamageRange.new(4500, 5000, Item.DamageType.Magical)
+)
+
+var ITEM_THUNDERFURY := Item.from(
+	func(item: Item) -> void:
+		item.name = "Thunderfury, Blessed Blade of the Windseeker"
+		item.quality = Item.ItemQuality.Legendary
+		item.description = "Probably a copyright infringement."
+		item.texture = preload("res://sprites/stone_sword_transparent_32x32.png")
+		item.stats = Stats.from([
+			Stats.StatValue.new(Stats.Stat.Strength, 33),
+			Stats.StatValue.new(Stats.Stat.Stamina, 27),
+			Stats.StatValue.new(Stats.Stat.Intellect, 15),
+			Stats.StatValue.new(Stats.Stat.Dexterity, 17),
+			Stats.StatValue.new(Stats.Stat.CriticalRating, 43),
+			Stats.StatValue.new(Stats.Stat.CriticalPower, 152),
+			Stats.StatValue.new(Stats.Stat.DodgeRating, 72),
+			Stats.StatValue.new(Stats.Stat.MagicArmor, 52),
+		])
+		item.slot = Item.EquipmentSlot.Weapon
+		item.weapon_slot = Item.WeaponSlot.OneHand
+		item.damage = Item.DamageRange.new(230, 270, Item.DamageType.Physical)
 )
 
 
@@ -98,4 +113,5 @@ var items : Array[Item] = [
 	ITEM_ROUGH_STONE,
 	ITEM_STONE_SWORD,
 	ITEM_THUNDERFURY,
+	ITEM_SABIUTA_LU_MARIUCA,
 ]
--- util/item.gd
+++ util/item.gd
@@ -9,6 +9,43 @@ enum ItemQuality {
 	Legendary
 }
 
+enum EquipmentSlot {
+	None,
+	Head,
+	Chest,
+	Shoulders,
+	Legs,
+	Feet,
+	Hands,
+	Neck,
+	Finger,
+	Ear,
+	Weapon,
+}
+
+enum WeaponSlot {
+	None,
+	OneHand,
+	OffHand,
+	MainHand,
+	TwoHand
+}
+
+enum DamageType {
+	Physical,
+	Magical
+}
+
+class DamageRange:
+	var min: int
+	var max: int
+	var type: DamageType
+	
+	func _init(from: int, to: int, typ: DamageType):
+		self.min = from
+		self.max = to
+		self.type = typ
+
 var id: int
 var name: String
 var description: String
@@ -16,15 +53,27 @@ var stack_size: int
 var texture: Texture2D
 var quality: ItemQuality
 var stats: Stats
+var slot: EquipmentSlot
+var weapon_slot: WeaponSlot
+var damage: DamageRange
 
-func _init(_id: int, _name: String, _quality: ItemQuality, _description: String, _stack_size: int, _texture: Texture2D, _stats: Stats = Stats.new()):
-	id = _id
-	quality = _quality
-	name = _name
-	description = _description
-	stack_size = _stack_size
-	texture = _texture if _texture != null else load("res://icon.svg")
-	stats = _stats
+static var last_id := 0
+
+func _init():
+	last_id += 1
+	id = last_id
+	texture = load("res://icon.svg")
+	stats = Stats.new()
+	quality = ItemQuality.Junk
+	slot = EquipmentSlot.None
+	stack_size = Consts.UNSTACKABLE
+	weapon_slot = WeaponSlot.None
+	damage = null
+
+static func from(cb: Callable) -> Item:
+	var item: Item = new()
+	cb.call(item)
+	return item
 	
 func _to_string() -> String:
 	return "Item(id: %d, name: '%s', quality: %s, description: '%s', stack_size: %d)" % [id, name, quality, description, stack_size]
